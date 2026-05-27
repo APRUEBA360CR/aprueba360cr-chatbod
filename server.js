0005c6b5e27f2ec39e6e39aee55a9795879f5448
APRUEBA360CR <arteantiguatattooalajuela@hotmail.com>
2026-05-27 00:16:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -29,10 +29,14 @@ app.post("/webhook-whatsapp", async (req, res) => {
     
         const urlZapi = "https://api.z-api.io/instances/3F3B7211DB8141B4C96832FB845BEA5C/token/6108C6AB5E8B2ECF2DBDD3D2/send-text";
 
-        await axios.post(urlZapi, {
-            phone: telefono,
-            message: respuesta
-        });
+       await axios.post(urlZapi, {
+    phone: telefono,
+    message: respuesta
+}, {
+    headers: {
+        'client-token': '6108C6AB5E8B2ECF2DBDD3D2'
+    }
+});
 
         res.sendStatus(200);
     } catch (e) {
